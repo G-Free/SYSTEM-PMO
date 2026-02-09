894b45c1663cec399ddd9ed4b598d8e0389e1448
Geiger Carlos <geiger.carlos15@gmail.com>
2026-02-09 11:01:59 +0100

    Update Login.tsx

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { authService } from "../services/authService";
 import { User } from "../types";
 import { SgaLogo } from "./icons/SgaLogo";
-import Sgalogotipo from "./components/conteudo/imagem/sga_logo.png";
+import Sgalogotipo from "components/conteudo/imagem/sga_logo.png";
 
 interface LoginProps {
   onLoginSuccess: (user: User) => void;
@@ -51,8 +51,7 @@ const Login: React.FC<LoginProps> = ({ onLoginSuccess }) => {
         <div className="relative z-20 flex flex-col justify-end p-16 h-full text-white">
           <div className="mb-6">
             <div className="h-50 w-50 bg-white p-8 rounded-xl flex items-center justify-center shadow-6xl shadow-white/50 mb-10">
-              <img src={Sgalogotipo} alt="" />
-              <img src={Sgalogotipo} alt="SGA Logo" className="h-12" />
+              <img src={Sgalogotipo} alt="SGA Logo" />
             </div>
             <h1 className="text-4xl font-bold tracking-tight mb-2">
               Excelência Operacional
